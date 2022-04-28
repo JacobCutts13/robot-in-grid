cf3f5b68190a3684167f0c5d9372e4ffa07a8a32
unknown <jacobdcutts@gmail.com>
2022-04-28 11:59:51 +0100

    working

--- src/index.ts
+++ src/index.ts
@@ -44,13 +44,46 @@ function NEXT_MOVE(GRID, POSITION, MOVES)
   GRID[CURRENT_POSITION] = "x" //don't go back that way
   POSITION move -MOVES[LAST_ELEMENT].direction //move backwards
   pop last MOVES
-  
+
 
 
  */
+
+function Move(grid: Grid, position: Position, path: Path, startPos: Position, targetPos: Position): Path | null{
+
+  if(position.x === targetPos.x && position.y === targetPos.y){
+    return path.slice(1);  
+  }
+  else{ 
+  if(cellAt({x: position.x + 1, y: position.y}, grid) === "."){
+    position.x += 1
+    const newPos = {x: position.x, y: position.y}
+    path.push(["right", newPos]);
+  }
+  else if(cellAt({x: position.x, y: position.y + 1}, grid) === "."){
+    position.y += 1
+    const newPos = {x: position.x, y: position.y}
+    path.push(["down", newPos]);
+  }
+  else{ 
+      if(position.x === startPos.x && position.y === startPos.y){
+      return null
+    }
+    else{
+      grid.rows[position.y][position.x] = "x"; //remove square
+      position.x = path[path.length -2][1].x;
+      position.y = path[path.length -2][1].y;
+      path.pop();
+      }
+    }
+  return Move(grid, position, path, startPos, targetPos);
+  }
+}
+
 export function solve(grid: Grid): Path | null {
   const targetPos = getBottomRightPosition(grid);
   const startPos = getTopLeftPosition(grid);
-  const answer = null;
+  const moveStartPos = {x: startPos.x, y: startPos.y}
+  const answer = Move(grid, moveStartPos, [["right", {x: startPos.x, y: startPos.y}]], startPos, targetPos);
   return answer;
 }
